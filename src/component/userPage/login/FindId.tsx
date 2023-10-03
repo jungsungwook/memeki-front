38a86b9375d71fd156173f7064934156f4e1b525
joeunSong <thdwhdms99@naver.com>
2023-10-03 16:25:07 +0900

    Feat - FindId & FindPw page UI (#1)

--- src/component/userPage/login/FindId.tsx
+++ src/component/userPage/login/FindId.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  LoginInput,
+  LoginInputList,
+  ModalPage,
+  TextButton,
+  TextButtonList,
+} from '../emotion/component';
+import { ButtonBox } from '../../emotion/component';
+import { Header1 } from '../../emotion/GlobalStyle';
 
 const FindId = () => {
-  return <div>FindId</div>;
+  return (
+    <ModalPage>
+      <Header1>아이디 찾기</Header1>
+      <LoginInputList>
+        <LoginInput type="email" placeholder="이메일" />
+      </LoginInputList>
+      <ButtonBox type="long">이메일 전송</ButtonBox>
+      <TextButtonList>
+        <TextButton type="pw" />
+        <TextButton type="login" />
+        <TextButton type="signUp" noLine />
+      </TextButtonList>
+    </ModalPage>
+  );
 };
 
 export default FindId;
--- src/component/userPage/login/FindPw.tsx
+++ src/component/userPage/login/FindPw.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import {
+  LoginInput,
+  LoginInputList,
+  ModalPage,
+  TextButton,
+  TextButtonList,
+} from '../emotion/component';
+import { ButtonBox, Line } from '../../emotion/component';
+import { Header1 } from '../../emotion/GlobalStyle';
 
 const FindPw = () => {
-  return <div>FindPw</div>;
+  return (
+    <ModalPage>
+      <Header1>비밀번호 찾기</Header1>
+      <LoginInputList>
+        <LoginInput type="user" placeholder="아이디" />
+        <Line />
+        <LoginInput type="email" placeholder="이메일" />
+      </LoginInputList>
+      <ButtonBox type="long">이메일 전송</ButtonBox>
+      <TextButtonList>
+        <TextButton type="id" />
+        <TextButton type="login" />
+        <TextButton type="signUp" noLine />
+      </TextButtonList>
+    </ModalPage>
+  );
 };
 
 export default FindPw;
